67682bf86967d04ffda963b167b3422ec611ed94
Pamidu jayaranga <pamidu@live.com>
2016-06-21 12:02:00 +0530

    lates to prodution

---
+++ latestToProduction.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+BuildOutPath="$PWD"
+username="duobuilduser"
+emailaddress="duobuilduser@duosoftware.com"
+password="DuoS12345"
+
+if [ "$1" = "-i" ]; then
+	apt-get update
+	#apt-get install -y python-software-properties  # 12.04
+	#add-apt-repository ppa:duh/golang
+	sudo apt-get install -y python-software-properties python g++ make
+	sudo add-apt-repository ppa:chris-lea/node.js
+	sudo apt-get update
+	#sudo apt-get install golang
+	sudo apt-get install -y nodejs nodejs-legacy  apache2 php5 php5-curl
+	sudo apt-get update
+	sudo apt-get install -y npm
+	sudo npm install -g bower
+fi
+
+#nodejs 
+git config --global user.name $username
+git config --global user.email $emailaddress
+echo "BEGIN REPO PULL Duoworldsite"
+if [ ! -d "Duoworldsite" ]; then
+#mkdir "depo"
+#cd depo
+git clone https://$username:$password@github.com/DuoSoftware/DuoworldProductionReady
+#cd ../
+fi
+cd Duoworldsite
+git pull
+sudo cp -r * /var/www/html
+cd ..
+
+#bower install --allow-root
+
+echo "Site Installed successfuly"
